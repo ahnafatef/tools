55df1aa01a86c2aa822b822cea49d77cd19361c3
p0rcup1ne <frankiecara101@gmail.com>
2017-10-01 21:53:41 +0600

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+# !/bin/bash
+
+# place this script in ~/bin and create a sym link
+# in the same directory under the name 'compile'
+# or whatever you feel like
 
 FILE=`echo $1 | egrep -o [^\.cpp]+`
 g++ $1 -o $FILE
